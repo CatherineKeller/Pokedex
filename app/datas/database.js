c299ee8a84522ed5812d06edef3ab749baa3a470
CatherineKeller <catherine.keller.oclock@gmail.com>
2023-01-05 12:58:52 +0100

    fix: connexion BDD

--- app/datas/database.js
+++ app/datas/database.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.DATABASE_URL;
 
 const client = new Client({
-  connectionString,
+  // connectionString,
 });
 
 client.connect();
